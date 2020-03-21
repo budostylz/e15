c37bc2554362c939d0f15bad0e057aab0402e84f
budostylz <lewis.shaun1@outlook.com>
2020-03-20 21:30:19 -0400

    UPDATE dictionary

--- e15p2/resources/views/drinks/dictionary.blade.php
+++ e15p2/resources/views/drinks/dictionary.blade.php
@@ -3,28 +3,33 @@
 
 @section('dictionary')
 
-    <div>Dictionary Results</div>
+    <div class="grid-container-nav">
+        <div class="grid-item drinkResults">
+            <div>
+                <ul class="nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="/">Back to Order</a>
+                    </li>          
+                </ul>
+            </div>
+            <hr/>
+        </div>
+    </div>
+
 
     @if(count($drinkArr) == 0)
         <strong>No Drinks Here</strong>
 
     @else
-        @foreach($drinkArr as $slug => $drink)
 
-             <label id="drinkName"> {{ $drink['strDrink'] }} </label>
-             <br/>
-
-             <div> 
-                <img src="{{ $drink['strDrinkThumb'] }}" height="200" width="200">                       
-             </div>
-
-             
-        @endforeach
-
-
+            <div class="grid-container-dict">
+                 @foreach( $drinkArr as $index => $drink )
+                       <div class="grid-item-dict">
+                            <label id="drinkName">{{ $drink['strDrink'] }} </label>
+                            <br/>
+                            <img src="{{ $drink['strDrinkThumb'] }}" height="200" width="200">                       
+                       </div>
+                 @endforeach
+            </div>
     @endif
-
-    
-
-
 @endsection

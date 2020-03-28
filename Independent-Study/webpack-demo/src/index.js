805299aa32373ac5b33a8137ce9bf446c606ad15
budostylz <lewis.shaun1@outlook.com>
2020-03-28 19:38:56 -0400

    update independent study

--- Independent-Study/webpack-demo/src/index.js
+++ Independent-Study/webpack-demo/src/index.js
@@ -1,9 +1,24 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+import Data from './data.xml';
+
 
 function component() {
     const element = document.createElement('div');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    element.innerHTML = _.join(['Hello', 'webpack'], ' '); //Lodash, now imported by this script
+    element.classList.add('hello');//CSS <style> applied 
+
+    //Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+
+    console.log(Data);
+
 
     return element;
 }
--- Independent-Study/webpack-demo/webpack.config.js
+++ Independent-Study/webpack-demo/webpack.config.js
@@ -6,4 +6,40 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    module: {
+        rules: [
+            {//styles
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+            {//images
+                test: /\.(png|svg|jpg|gif)/,
+                use: [
+                    'file-loader',
+                ]
+            },
+            {// fonts
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader',
+                ]
+            },
+            {//csv, tsv
+                test: /\.(csv|tsv)/,
+                use: [
+                    'csv-loader',
+                ],
+            },
+            {//xml
+                test: /\.xml$/,
+                use: [
+                    'xml-loader',
+                ],
+            },
+
+        ],
+    },
 };

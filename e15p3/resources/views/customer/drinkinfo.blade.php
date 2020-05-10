f5fb24ffa6e1d7f86dbaa78b5795c2e7f955f535
budostylz <lewis.shaun1@outlook.com>
2020-05-09 22:27:02 -0400

    update forms and views

--- e15p3/resources/views/customer/drinkinfo.blade.php
+++ e15p3/resources/views/customer/drinkinfo.blade.php
@@ -21,31 +21,31 @@
 
             <div class="row pt-5">
                 <div class="col-sm">
-                    <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">Check to Order
+                    <input type="checkbox" id="drink1" name="drink1" value="drink1">Check to Order
                     <p class="text-left">Drink1<br/> $3.00</p>
                     <p class="text-left"><a href="/drinkdetails">Drink Details</a></p>
                     <img src="https://www.thecocktaildb.com/images/media/drink/yyrwty1468877498.jpg" alt="" style="width:200px;height:200px;">
                 </div>
                 <div class="col-sm">
-                    <input type="checkbox" id="vehicle12" name="vehicle12" value="Bike2">Check to Order
+                    <input type="checkbox" id="drink2" name="drink2" value="drink2">Check to Order
                     <p class="text-left">Drink2<br/> $3.00</p>
                     <p class="text-left"><a href="/drinkdetails">Drink Details</a></p>
                     <img src="https://www.thecocktaildb.com/images/media/drink/yyrwty1468877498.jpg" alt="" style="width:200px;height:200px;">
                 </div>
                 <div class="col-sm">
-                    <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">Check to Order
+                    <input type="checkbox" id="drink3" name="drink3" value="drink3">Check to Order
                     <p class="text-left">Drink3<br/> $3.00</p>
                     <p class="text-left"><a href="/drinkdetails">Drink Details</a></p>
                     <img src="https://www.thecocktaildb.com/images/media/drink/yyrwty1468877498.jpg" alt="" style="width:200px;height:200px;">
                 </div>
                 <div class="col-sm">
-                    <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">Check to Order
+                    <input type="checkbox" id="drink4" name="drink4" value="drink4">Check to Order
                     <p class="text-left">Drink4<br/> $3.00</p>
                     <p class="text-left"><a href="/drinkdetails">Drink Details</a></p>
                     <img src="https://www.thecocktaildb.com/images/media/drink/yyrwty1468877498.jpg" alt="" style="width:200px;height:200px;">
                 </div>
                 <div class="col-sm">
-                    <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">Check to Order
+                    <input type="checkbox" id="drink5" name="drink5" value="drink5">Check to Order
                     <p class="text-left">Drink5<br/> $3.00</p>
                     <p class="text-left"><a href="/drinkdetails">Drink Details</a></p>
                     <img src="https://www.thecocktaildb.com/images/media/drink/yyrwty1468877498.jpg" alt="" style="width:200px;height:200px;">
@@ -56,31 +56,31 @@
 
             <div class="row pt-5">
                 <div class="col-sm">
-                    <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">Check to Order
+                    <input type="checkbox" id="drink6" name="drink6" value="drink6">Check to Order
                     <p class="text-left">Drink6<br/> $3.00</p>
                     <p class="text-left"><a href="/drinkdetails">Drink Details</a></p>
                     <img src="https://www.thecocktaildb.com/images/media/drink/yyrwty1468877498.jpg" alt="" style="width:200px;height:200px;">
                 </div>
                 <div class="col-sm">
-                    <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">Check to Order
+                    <input type="checkbox" id="drink7" name="drink7" value="drink7">Check to Order
                     <p class="text-left">Drink7<br/> $3.00</p>
                     <p class="text-left"><a href="/drinkdetails">Drink Details</a></p>
                     <img src="https://www.thecocktaildb.com/images/media/drink/yyrwty1468877498.jpg" alt="" style="width:200px;height:200px;">
                 </div>
                 <div class="col-sm">
-                    <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">Check to Order
+                    <input type="checkbox" id="drink8" name="drink8" value="drink8">Check to Order
                     <p class="text-left">Drink8<br/> $3.00</p>
                     <p class="text-left"><a href="/drinkdetails">Drink Details</a></p>
                     <img src="https://www.thecocktaildb.com/images/media/drink/yyrwty1468877498.jpg" alt="" style="width:200px;height:200px;">
                 </div>
                 <div class="col-sm">
-                    <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">Check to Order
+                    <input type="checkbox" id="drink9" name="drink9" value="drink9">Check to Order
                     <p class="text-left">Drink9<br/> $3.00</p>
                     <p class="text-left"><a href="/drinkdetails">Drink Details</a></p>
                     <img src="https://www.thecocktaildb.com/images/media/drink/yyrwty1468877498.jpg" alt="" style="width:200px;height:200px;">
                 </div>
                 <div class="col-sm">
-                    <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">Check to Order
+                    <input type="checkbox" id="drink10" name="vdrink10" value="drink10">Check to Order
                     <p class="text-left">Drink10<br/> $3.00</p>
                     <p class="text-left"><a href="/drinkdetails">Drink Details</a></p>
                     <img src="https://www.thecocktaildb.com/images/media/drink/yyrwty1468877498.jpg" alt="" style="width:200px;height:200px;">

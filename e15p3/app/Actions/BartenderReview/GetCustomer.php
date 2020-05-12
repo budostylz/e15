260ea62968160cd738ba2757cd526ea490a6b665
budostylz <lewis.shaun1@outlook.com>
2020-05-12 08:09:35 -0400

    prod deployment

--- e15p3/app/Actions/BartenderReview/GetCustomer.php
+++ e15p3/app/Actions/BartenderReview/GetCustomer.php
@@ -10,21 +10,29 @@ class GetCustomer
     public function __construct()
     {
 
-        $orders = Order::distinct()
-            ->select('user_id')
-            ->where('status', '=', null)
-            ->orderBy('updated_at', 'desc')
-            ->get();
-
+        $userOrder = Order::distinct()->select('user_id')->get();
+        $orders = $userOrder->where('status', '=', null);
+        
+        //where('status', '=', null)->orderBy('updated_at', 'desc');
+        //$userArr = array();
+        //Order::dump();
+        dump("Order::distinct()->select('user_id')->get()");
+        //dump( Order::distinct()->select('user_id')->where('status', '=', null)->orderBy('updated_at', 'desc')->get() );
         dump($orders);
-
         foreach ($orders as $order) {
+
+            //dump($order->user_id);
             $user_id = $order->user_id;
             $user_title = User::where('id', '=', $user_id)->get()->first()->user_name;
             $userArr[$user_id] = $user_title;
+            dump($user_title);
+
+
         }
 
         $this->userArr = $userArr;
+
+        dump($this->userArr);
 
     }
 }
